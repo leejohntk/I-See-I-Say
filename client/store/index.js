52da7c66616f1ef0b3feaf6ce4dba273b6415183
John Lee <lee.john.tk@gmail.com>
2021-08-31 20:28:21 -0500

    removed auth

--- client/store/index.js
+++ client/store/index.js
@@ -2,17 +2,15 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
 import translate from './translate'
 import vision from './vision'
 
-const reducer = combineReducers({ auth, translate, vision })
+const reducer = combineReducers({ translate, vision })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 const store = createStore(reducer, middleware)
 
 export default store
-export * from './auth'
 export * from './translate'
 export * from './vision'
